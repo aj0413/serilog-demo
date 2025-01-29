a9d23f82478ca4ef73c6716cf9272b640406d7ce
Jonathan Allbritten <18425821+aj0413@users.noreply.github.com>
2025-01-29 00:09:38 -0500

    expand demo

--- Program.cs
+++ Program.cs
@@ -1,24 +1,66 @@
 using System.Linq.Expressions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using serilog_demo;
 
+// Creates initial ReloadableLogger for use before access to configuration or services
+// Will be reconfigured later, unless specified to be preserved
+// This is a 'short' lived logger meant to be used via Serilog.Log before other configuration loads
 Log.Logger = new LoggerConfiguration()
     .WriteTo.Console()
+    .WriteTo.File(
+        path: "./logs/bootstrap/log-.json",
+        rollingInterval: RollingInterval.Day,
+        formatter: new Serilog.Formatting.Compact.CompactJsonFormatter())
     .CreateBootstrapLogger();
+
+// Diagnose Serilog itself
+Serilog.Debugging.SelfLog.Enable(Console.Error);
 
 try
 {
+    Log.Information("Application starting up...");
+
     var builder = WebApplication.CreateBuilder(args);
 
+    Host.CreateApplicationBuilder();
+
+    // Alternative way to register Serilog as only provider; acts as thin wrapper for below method
+    // Only works with IHostBuilder which is unique to web applications
+    // builder.Host.UseSerilog((host, lc) => lc
+    //     .ReadFrom.Configuration(builder.Configuration));
+
+    // Prefered way to register Serilog as only provider
+    // Works with all application/builder models
     builder.Services.AddSerilog((lc) => lc
-        .ReadFrom.Configuration(builder.Configuration));
+        .ReadFrom.Configuration(builder.Configuration),
+        // Set to true to see Serilog.Log continue to use settings set above
+        // Othewise, Serilog.Log will be reconfigured based on these settings
+        preserveStaticLogger: false,
+        // Set to true to see Serilog preserve the other log providers registered via builder.Logging
+        //  Otherwise, Serilog will ignore all other providers
+        writeToProviders: false);
 
     builder.Services.AddDbContext<WeatherForecastContext>(options => 
+        // EnableSenstiveDataLogging isn't necessary here aside from making it query parameter values are actually shown in logs
         options.UseSqlite($"Data Source=./weatherforecast.db").EnableSensitiveDataLogging()); 
+
+    // Adds Serilog along with other log providers
+    // Notice logs will still go to console (add be formatted differently) because you're now using the default log providers+Serilog
+    // builder.Logging
+    //     .AddSerilog(
+    //         new LoggerConfiguration()
+    //             .WriteTo.File(
+    //                 path: "./logs/another-provider/log-.json",
+    //                 rollingInterval: RollingInterval.Day,
+    //                 formatter: new Serilog.Formatting.Compact.CompactJsonFormatter())
+    //             .CreateLogger()
+    //         );
 
     var app = builder.Build();
 
+    // Comment out if testing builder.Logging.AddSerilog as the required middleware will be missing
     app.UseSerilogRequestLogging();
     app.UseHttpsRedirection();
 
@@ -29,12 +71,32 @@ try
         WeatherForecastContext context,
         CancellationToken ct) =>
     {
-        logger.LogInformation("Test log 1..2..3");
+        logger.LogInformation("Calling endpoint for weather stuffs");
 
         var query = context.WeatherForecasts.WhereIf(!string.IsNullOrWhiteSpace(summary), w => w.Summary == summary);
         query = query.WhereIf(temperatureC is int, w => w.TemperatureC >= temperatureC);
 
         return await query.ToListAsync(ct);
+    });
+
+    app.MapPost("/throw-error", () =>
+    {
+        throw new Exception("Exception handling test...");
+    });
+
+    app.MapPost("/test-loggers", (
+        ILogger<Program> normalLogger,
+        Serilog.ILogger serilogAbstractLogger) =>
+    {
+        normalLogger.LogInformation("Testing {type} 1..2..3", normalLogger.GetType());
+
+        serilogAbstractLogger.Information("Testing {type} 1..2..3", serilogAbstractLogger.GetType());
+
+        // This grabs the static/singleton Serilog.Log instance
+        // See how the type changes based on preserveStaticLogger
+        var staticLogger = Log.ForContext<Program>();
+
+        staticLogger.Information("Testing {type} 1..2..3", staticLogger.GetType());
     });
 
     app.Run();
@@ -45,6 +107,9 @@ catch (Exception exception)
 }
 finally
 {
+    // Notice that this log ends up going with the rest of the logs instead of inside of ./logs/bootstrap/log-.json
+    // Will use configuration for intial BoostrapLogger if preserveStaticLogger: true
+    Log.Information("Closing and flushing logger");
     Log.CloseAndFlush();
 }
 
--- README.md
+++ README.md
@@ -1,20 +1,72 @@
 # Demo of using Serilog in .Net 9
 
+This repository is to act as a learning tool and demo for Serilog and logging in general.
 
-## Different ways to register Serilog as the logging provider
+This should (hoepfully) cover most normal topics and use-cases.
 
-specific to IHostBuilder interface
+## Registering Serilog for use
+
+There's actually multiple ways to register serilog. Depending on the intention:
+
+- [Only Log Provider](#serilog-as-the-sole-log-provider-at-runtime)
+
+### Serilog as the sole log provider at runtime.
+
+This is the normal and intended use-case for serilog. Generally speaking, you should be using Serilog.Sinks to log to different places as their equivalent.
+
+Confusingly, this two ways to do this at the moment.
+
+If you're working in web app and are using  `WebApplication.CreateBuilder` then you have access to `IHostBuilder` via `builder.Host`:
 
 ```
+var builder = WebApplication.CreateBuilder(args);
+
 builder.Host.UseSerilog((host, lc) => lc
     .ReadFrom.Configuration(builder.Configuration));
 ```
 
-above is functionally equivalent to the following (it ultimately calls it internally), see source code:
+If you *don't* have access to `IHostBuilder`, such as a non-web app, then you need to do the following:
+
+```
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddSerilog((lc) => lc
+     .ReadFrom.Configuration(builder.Configuration));
+```
+
+Note, however, that the latter is actually an extension on `IServiceCollection` and can thus be called in both cases. Furthermore, the first example is just a thin wrapper over it. 
+
+This is why it's the new default in the documentation/examples.
+
+See:
+- [GitHub Issue: UseSerilog support for .net 7 CreateApplicationBuilder and HostApplicationBuilder](https://github.com/serilog/serilog/issues/1855)
+- [GitHub Issue: UseSerilog support for .net8 IHostApplicationBuilder](https://github.com/serilog/serilog-extensions-hosting/issues/76)
+
+Source code:
 - [IHostBuilder.UseSerilog](https://github.com/serilog/serilog-extensions-hosting/blob/87e316f7d31ae431747d1106976dfceffdecc32c/src/Serilog.Extensions.Hosting/SerilogHostBuilderExtensions.cs#L100)
 - [IServiceCollection.AddSerilog](https://github.com/serilog/serilog-extensions-hosting/blob/87e316f7d31ae431747d1106976dfceffdecc32c/src/Serilog.Extensions.Hosting/SerilogServiceCollectionExtensions.cs#L129)
 
+
+### Serilog as one of N log providers
+
+This is a more abnormal setup, but may come up if a ready Serilog.Sink does not exist for the service you wish to send logs to, but *does* have docs on registering a log provider.
+
+For instance, if you want to capture logs with [Application Insights](https://learn.microsoft.com/en-us/azure/azure-monitor/app/ilogger).
+
 ```
-builder.Services.AddSerilog((lc) => lc
-     .ReadFrom.Configuration(builder.Configuration));
+builder.Logging.AddApplicationInsights(
+        configureTelemetryConfiguration: (config) => 
+            config.ConnectionString = builder.Configuration.GetConnectionString("APPLICATIONINSIGHTS_CONNECTION_STRING"),
+            configureApplicationInsightsLoggerOptions: (options) => { }
+    );
+```
+
+In this case, you'd register Serilog via:
+
+```
+builder.Logging.AddSerilog(
+    new LoggerConfiguration()
+        .ReadFrom.Configuration(builder.Configuration)
+        .CreateLogger()
+    );
 ```
