15c094728bb76d40b8f08dc74f41b9693c28a669
Jonathan Allbritten <18425821+aj0413@users.noreply.github.com>
2025-01-28 20:04:08 -0500

    integrate better ef log examples

--- Program.cs
+++ Program.cs
@@ -1,28 +1,55 @@
+using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using serilog_demo;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddSerilog((lc) => lc
-    .ReadFrom.Configuration(builder.Configuration));
-
-builder.Services.AddDbContext<WeatherForecastContext>(options => 
-    options.UseSqlite($"Data Source=./weatherforecast.db")); 
-
-var app = builder.Build();
-
-app.UseSerilogRequestLogging();
-app.UseHttpsRedirection();
-
-app.MapGet("/weatherforecast", async (
-    ILogger<Program> logger,
-    WeatherForecastContext context,
-    CancellationToken ct) =>
+try
 {
-    logger.LogInformation("Test log 1..2..3");
+    var builder = WebApplication.CreateBuilder(args);
 
-    return await context.WeatherForecasts.ToListAsync(ct);
-});
+    builder.Services.AddSerilog((lc) => lc
+        .ReadFrom.Configuration(builder.Configuration));
 
-app.Run();
+    builder.Services.AddDbContext<WeatherForecastContext>(options => 
+        options.UseSqlite($"Data Source=./weatherforecast.db").EnableSensitiveDataLogging()); 
+
+    var app = builder.Build();
+
+    app.UseSerilogRequestLogging();
+    app.UseHttpsRedirection();
+
+    app.MapGet("/weatherforecast", async (
+        string? summary,
+        int? temperatureC,
+        ILogger<Program> logger,
+        WeatherForecastContext context,
+        CancellationToken ct) =>
+    {
+        logger.LogInformation("Test log 1..2..3");
+
+        var query = context.WeatherForecasts.WhereIf(!string.IsNullOrWhiteSpace(summary), w => w.Summary == summary);
+        query = query.WhereIf(temperatureC is int, w => w.TemperatureC >= temperatureC);
+
+        return await query.ToListAsync(ct);
+    });
+
+    app.Run();
+}
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+static class Extensions
+{
+    public static IQueryable<T> WhereIf<T>(this IQueryable<T> query, bool condition, Expression<Func<T, bool>> predicate) =>
+        condition ? query.Where(predicate) : query;
+}
